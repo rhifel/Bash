d16c9ed5af860aaa42c94ce355f024bd45ee71c2
rhifel <161362805+rhifel@users.noreply.github.com>
2025-07-07 21:04:50 +0800

    just practicing some stuff maybe I'll put the basics of bash here

---
+++ start.sh
@@ -0,0 +1,31 @@
+# echo "$HOME"
+# date +"%Y-%m-%d %H-%M-%S"
+
+# name="Rhifel Mar A. Buscado"
+
+# echo "$name"
+
+# read -r -p "What is your Name? " name
+# echo "$name"
+
+PASSWORD="YWRtaW4xMjM="
+USERNAME="TGV0bWVJbg=="
+
+login() {
+    echo "====== System Login ======"
+    read -p "Username: " username
+    encoded_user=$(echo -n "$username" | base64)
+    read -s -p "Password: " password_input
+    encoded_pass=$(echo -n "$password_input" | base64)
+    echo
+    if [[ "$encoded_user" == "$USERNAME" && "$encoded_pass" == "$PASSWORD" ]]; then
+        echo "Login successful."
+    #     echo "$(date) LOGIN SUCCESS: $username" >> "$LOG_FILE"
+    # else
+    #     echo "Login failed."
+    #     echo "$(date) LOGIN FAILED: $username" >> "$LOG_FILE"
+    #     exit 1
+    fi
+}
+
+login
